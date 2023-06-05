b1271df57b731ea56e8383b4b54428cafadf96c6
superfast852 <superfast852@gmail.com>
2023-06-04 21:30:50 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import pigpio
+from simple_pid import PID
+
+pi = pigpio.pi()
+
+
+def in_tolerance(a, b, tolerance=10):
+    return (b-tolerance)< a >(b+tolerance)
+
+
+class Motor:
+    def __init__(self, pwm, dir, a, b, p, i, d):
+        self.encoder = Encoder(a, b)
+        self.speed = 0
+        pi.set_mode(dir, pigpio.OUTPUT)
+        pi.set_PWM_range(pwm, 100)
+        self._pwm = pwm
+        self.dir = dir
+        self.pid_params = (p, i, d)
+
+    def set(self, speed):
+        pi.set_PWM_dutycycle(self._pwm, abs(speed*100))
+        pi.write(self.dir, speed > 0)
+
+    def setPosition(self, ticks):
+        p, i, d = self.pid_params
+        pid = PID(p, i, d, ticks, output_limits=(-1, 1), sample_time=0.05)
+        out = []
+        while not in_tolerance(self.encoder.ticks, ticks):
+            v = pid(self.encoder.ticks)
+            self.set(v)
+            out.append(v)
+        return out
+
+
+class Encoder:
+    def __init__(self, a, b):
+        self.callback = pi.callback(a, 0, self._update)
+        self.b = b
+        self.ticks = 0
+        self.lastTick = 0
+        self.speed = 0
+
+    def _update(self, gpio, level, tick):
+        if self.lastTick == 0:
+            self.lastTick = tick
+        else:
+            elapsed = tick-self.lastTick
+            self.lastTick = tick
+            self.speed = 1_000_000/elapsed
+        if pi.read(self.b):
+            self.ticks += 1
+        else:
+            self.ticks -= 1
+
+
+if __name__ == "__main__":
+    motor = Motor(13, 6, 27, 17, 0.5, 0.5, 1)
